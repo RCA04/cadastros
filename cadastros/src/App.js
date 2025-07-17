3a15f9b014f2001eeae18383e1e437400ff293a2
RCA04 <ryan04.dev@gmail.com>
2025-07-17 00:37:42 -0300

    develop: inserção do furmulário de cadastro

--- cadastros/src/App.js
+++ cadastros/src/App.js
@@ -2,62 +2,95 @@ import { useState } from "react";
 import axios from "axios";
 
 function App() {
-
-  const [form, setForm] = useState({
-
+const [form, setForm] = useState({
 nome: "",
-
 sobrenome: "",
-
 cpf: "",
-
 telefone: "",
-
 });
 
 const [mensagem, setMensagem] = useState("");
-
 const handleChange = (e) => {
-
 setForm({ ...form, [e.target.name]: e.target.value });
-
 };
 
 const handleSubmit = async (e) => {
-
 e.preventDefault();
-
 try {
-
 await axios.post("http://localhost:8000/api/pessoas", form);
 
 setMensagem("Pessoa cadastrada com sucesso!");
 
 setForm({
-
 nome: "",
-
 sobrenome: "",
-
 cpf: "",
-
 telefone: "",
-
 });
 
 } catch (error) {
 
 setMensagem("Erro ao cadastrar pessoa.");
 
-alert.error(error.response?.data || error.message);
+alert(error.response?.data || error.message);
 
-}
+}}
 
-  return (
-    <div className="container">
-      
-    </div>
-  );
-}
+return (
+<div className="container">
+
+<h2>Cadastro</h2>
+
+<form onSubmit={handleSubmit}>
+
+<input
+type="text"
+name="nome"
+placeholder="Nome"
+value={form.nome}
+onChange={handleChange}
+required
+/>
+
+<input
+type="text"
+name="sobrenome"
+placeholder="Sobre Nome"
+value={form.sobrenome}
+onChange={handleChange}
+required
+/>
+
+<input
+type="text"
+name="cpf"
+placeholder="CPF"
+value={form.cpf}
+onChange={handleChange}
+minLength='11'
+maxLength='11'
+required
+/>
+
+<input
+type="text"
+name="telefone"
+placeholder="Telefone"
+value={form.telefone}
+onChange={handleChange}
+minLength='11'
+maxLength='11'
+required
+/>
+
+<button type="submit">Cadastrar</button>
+
+</form>
+
+{mensagem && <p>{mensagem}</p>}
+    </div> 
+    
+);}
+
 
 export default App;
