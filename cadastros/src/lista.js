29766c3914a66ab0d85de1dbff6f6d1aae5492a1
RCA04 <ryan04.dev@gmail.com>
2025-07-17 14:03:09 -0300

    inclusão do sistema de Get da api

--- cadastros/src/lista.js
+++ cadastros/src/lista.js
@@ -1,13 +1,19 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
 
 
 function Lista(){
 
 const api = process.env.REACT_APP_API_URL
-axios.get(api);
 
-console.log(api);
+const [usuarios, setUsuarios] = useState([]);
 
+useEffect(()=>{
+    axios.get(api)
+    .then(response=>{
+        setUsuarios(response.data)
+    })
+}, [])
 
     return(
         <div>
